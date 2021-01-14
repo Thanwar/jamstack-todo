9f57f3b7fa5ea5edf02165950001d1dff5621a91
Thanwar <thawardas25@gmail.com>
2021-01-15 00:57:12 +0500

    added todos api

--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -1,4 +1,21 @@
 const React = require("react");
 const wrapRootElement = require("./wrap-root-element");
 
-exports.wrapRootElement = wrapRootElement;
+const {
+    ApolloProvider,
+    ApolloClient,
+    InMemoryCache,
+  } = require("@apollo/client");
+  
+  const client = new ApolloClient({
+    uri: "https://thanwar-todos.netlify.app/.netlify/functions/graphql",
+    cache: new InMemoryCache(),
+  });
+
+exports.wrapRootElement = ({ element }) => {
+    return (
+      <ApolloProvider client={client}>
+        {wrapRootElement({ element })}
+      </ApolloProvider>
+    );
+  };
--- packages/www/wrap-root-element.js
+++ packages/www/wrap-root-element.js
@@ -3,17 +3,6 @@ const { ThemeProvider } = require("theme-ui");
 const { deep } = require("@theme-ui/presets");
 const { Provider } = require("./netlifyIdentityContext");
 
-const {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-} = require("@apollo/client");
-
-const client = new ApolloClient({
-  uri: "https://thanwar-todos.netlify.app/.netlify/functions/graphql",
-  cache: new InMemoryCache(),
-});
-
 const newTheme = {
   ...deep,
   sizes: { container: 1024 },
@@ -21,8 +10,6 @@ const newTheme = {
 
 module.exports = ({ element }) => (
   <Provider>
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={newTheme}>{element}</ThemeProvider>
-    </ApolloProvider>
+    <ThemeProvider theme={newTheme}>{element}</ThemeProvider>
   </Provider>
 );
